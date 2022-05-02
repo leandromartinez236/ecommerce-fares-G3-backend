287e86995c08d37168bf25da29c8105323f1d20a
Ulises Acosta <ulisesdavidacosta27@gmail.com>
2022-05-02 22:09:57 +0200

    express server running

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.listen(3000);
+console.log("Server is running at port 3000");
