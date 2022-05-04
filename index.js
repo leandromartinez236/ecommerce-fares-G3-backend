44cc265e2cb2479b14fbaca10a733d36698cc141
leandro <martinezleandro2004@gmail.com>
2022-05-04 20:37:15 -0300

    chore: routes

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+
 app.use(productRoutes);
+
 app.listen(3000, () => console.log("Server corriendo"));
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const { Product } = require("../models/product");
 
 const getProducts = async (req, res) => {
   try {
@@ -8,7 +8,6 @@ const getProducts = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
-//name, description,price,stock
 const createProduct = async (req, res) => {
   try {
     const { name, description, price, stock } = req.body;
@@ -23,7 +22,55 @@ const createProduct = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+const putProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, stock } = req.body;
+    const product = await Product.findByPk(id);
+    product.name = name;
+    product.description = description;
+    product.stock = stock;
+    await product.save();
+
+    res.status(200).json(product);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Product.destroy({ where: { id } });
+    res.sendStatus(200);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+const getOneProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Product.findByPk(id);
+    res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+// const getProductQuery = async (req, res) => {
+//   try {
+//     // const { name, description } = req.query;
+//     // console.log(name, description);
+//     console.log(req.query);
+//     // const result = await Product.findByPk(id);
+//     // res.status(200).json(result);
+//   } catch (error) {
+//     return res.status(500).json({ message: error.message });
+//   }
+// };
 module.exports = {
   getProducts,
   createProduct,
+  deleteProduct,
+  putProduct,
+  getOneProduct,
+  // getProductQuery,
 };
